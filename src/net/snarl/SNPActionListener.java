7c8e7ad6f3c4e5215d1e28f004d87fd2401cf07b
Patrick von Reth <vonreth@kde.org>
2009-09-18 14:51:34 +0000

    new doc

--- src/net/snarl/SNPActionListener.java
+++ src/net/snarl/SNPActionListener.java
@@ -1,9 +1,24 @@
 package net.snarl;
 
-public interface  SNPActionListener {
+public interface SNPActionListener {
+	/**
+	 * Called if the Notification was left clicked
+	 */
 	public void notificationLeftClicked();
+
+	/**
+	 * Called if the Notification was right clicked
+	 */
 	public void notificationRightClicked();
+
+	/**
+	 * Called if the Notification was closed by the user
+	 */
 	public void notificationClosed();
+
+	/**
+	 * Called if the Notification timed out
+	 */
 	public void notificationTimedOut();
-	
+
 }
